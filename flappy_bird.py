a2a60e826d187b092f5cc6e298863e1451b67da5
Henrik0122 <overgaard22@gmail.com>
2024-11-11 11:19:10 +0100

    Update flappy_bird.py

--- flappy_bird.py
+++ flappy_bird.py
@@ -2,6 +2,8 @@ import pygame
 import random
 import time
 
+import pygame.surface
+
 
 class Entity:
     def __init__(self, x: float, y: float, velocity: float, sprite: pygame.surface) -> None:
@@ -22,12 +24,68 @@ class Player(Entity):
         super().__init__(x, y, velocity, sprite)
         self.gravity_constant = gravity_constant
 
+
     def update(self, dt) -> None:
         self.y += self.velocity * dt
         self.velocity += self.gravity_constant * dt
 
     def render(self, screen: pygame.surface) -> None:
         screen.blit(self.sprite, (self.x, self.y))
+
+class Obstacle(Entity):
+
+    class ObtacleBlocks(Entity):
+        def __init__(self, x: float, y: float, velocity: float, sprite: pygame.surface) -> None:
+            super().__init__(x, y, velocity, sprite)
+
+        def update(self, dt) -> None:
+            self.x += self.velocity * dt
+        
+        def render(self, screen: pygame.surface) -> None:
+            screen.blit(self.sprite, (self.x, self.y))
+        
+    def __init__(self, x: float,
+                y: float,
+                velocity: float,
+                screen_height: int,
+                gap_height: int,
+                gap_loc: int,
+                sprite: pygame.surface) -> None:
+        super().__init__(x, y, velocity, sprite)
+
+        self.screen_height = screen_height
+        self.gap_height = gap_height
+        self.gap_loc = gap_loc
+        self.BLOCK_SIZE = 48
+
+        #calculate the number of blocks required to fill the screen
+        self.num_blocks = round(self.screen_height/self.BLOCK_SIZE)
+
+        #calculate gap
+        self.gap_range = (self.gap_loc, self.gap_loc + self.gap_height)
+
+        self.blocks = self.create_blocks()
+
+    def create_blocks(self) -> list[ObtacleBlocks]:
+        o = []
+        current_block = 0
+        for i in range(self.num_blocks):
+            if i < self.gap_range[0] or i > self.gap_range[1]:
+                o.append(Obstacle.ObtacleBlocks(self.x,
+                                                current_block,
+                                                self.velocity,
+                                                self.sprite))
+                current_block += self.BLOCK_SIZE
+        return o
+    
+    def update(self, dt) -> None:
+        self.x += self.velocity * dt
+        for b in self.blocks:
+            b.update(dt)
+    
+    def render(self, screen: pygame.surface) -> None:
+        for b in self.blocks:
+            b.render(screen)
 
 
 class Game:
@@ -42,6 +100,7 @@ class Game:
         # GAME Constants
         self.GRAVITY_CONSTANT = 1700
         self.PLAYER_VEL = 200
+        self.JUMP_CONSTANT = -450
 
         self.player = Player(self.screen.get_width()/2,
                              self.screen.get_height()/2,
@@ -50,10 +109,21 @@ class Game:
                              self.GRAVITY_CONSTANT
                              )
 
+        self.test_obstacle = Obstacle(1280,
+                                      0,
+                                      -200,
+                                      self.screen.get_height(),
+                                      2,
+                                      5,
+                                      self.sprites["obstacle"])
     def poll_events(self) -> None:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 self.running = False
+
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    self.player.velocity = self.JUMP_CONSTANT
 
     def update(self) -> None:
         if self.previous_time is None:
@@ -65,11 +135,13 @@ class Game:
         self.previous_time = now
 
         self.player.update(dt)
+        self.test_obstacle.update(dt)
 
     def render(self) -> None:
         self.screen.fill("black")
 
         self.player.render(self.screen)
+        self.test_obstacle.render(self.screen)
 
         pygame.display.update()
 
@@ -83,12 +155,8 @@ class Game:
     def load_sprites(self) -> dict:
         sprites = {}
 
-<<<<<<< HEAD
         sprites["player"] = pygame.image.load(r"Games\gfx\ball.png").convert_alpha()
-
-=======
-        sprites["player"] = pygame.image.load(r"gfx\ball.png").convert_alpha()
->>>>>>> 6639394158aa56b0ebc164fac9c2059877dcaf77
+        sprites["obstacle"] = pygame.image.load(r"Games\gfx\block.png").convert_alpha()
 
         return sprites
 
